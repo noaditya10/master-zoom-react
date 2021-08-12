7b12683d9d1a2422f0c30aaf6334616bbfd81614
Satya <noaditya10@gmail.com>
2021-08-12 16:58:35 +0700

    menambah component ManajemenBuku

---
+++ src/components/ManajemenBuku.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ManajemenBuku() {
+  //data
+
+  //eventhandling
+
+  return (
+    <div className="container mt-3">
+      <h1 className="text-center">Manajemen Buku</h1>
+
+      <div id="formTambah"></div>
+      <div id="formUbah"></div>
+      <div id="daftarBuku"></div>
+    </div>
+  );
+}
+export default ManajemenBuku;
