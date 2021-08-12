48123158d8a6bc6d4c02270e262d58b281837045
Satya <noaditya10@gmail.com>
2021-08-12 17:27:02 +0700

    menampilkan Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ManajemenBuku from "./components/ManajemenBuku";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <ManajemenBuku />
     </div>
   );
