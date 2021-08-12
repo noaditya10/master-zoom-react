5f26cadc8a7fadbcf764118130292375b1c822c9
Satya <noaditya10@gmail.com>
2021-08-12 18:41:36 +0700

    interface UPDATE

--- src/components/ManajemenBuku.jsx
+++ src/components/ManajemenBuku.jsx
@@ -26,7 +26,23 @@ function ManajemenBuku({ bookList }) {
         </form>
       </div>
 
-      <div id="formUbah"></div>
+      <div id="formUbah">
+        <h5>Ubah Buku</h5>
+        <hr />
+        <form className="row">
+          <div className="col-3">
+            <input type="text" name="judul" className="form-control mx-2" placeholder="Judul" />
+          </div>
+          <div className="col-2">
+            <input type="number" name="stok" className="form-control mx-2" placeholder="Stok" />
+          </div>
+          <div className="col-2">
+            <input type="submit" className="btn btn-warning ml-2" value="Ubah" />
+            <input type="button" className="btn btn-danger ml-2" value="Batal" />
+          </div>
+        </form>
+      </div>
+
       <div id="daftarBuku">
         <h2 className="mt-3">Daftar Buku</h2>
         <hr />
