a8ce9df446c6aacbcafdd9f724d9558c6a76453a
Satya <noaditya10@gmail.com>
2021-08-12 18:32:30 +0700

    interface READ

--- src/components/ManajemenBuku.jsx
+++ src/components/ManajemenBuku.jsx
@@ -12,7 +12,34 @@ function ManajemenBuku({ bookList }) {
 
       <div id="formTambah"></div>
       <div id="formUbah"></div>
-      <div id="daftarBuku"></div>
+      <div id="daftarBuku">
+        <h2 className="mt-3">Daftar Buku</h2>
+        <hr />
+        <button className="btn btn-primary m-2">Tambah Buku</button>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Judul</th>
+              <th>Stok</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookList.map((book, index) => (
+              <tr key={index}>
+                <td> {index + 1}</td>
+                <td> {book.judul} </td>
+                <td> {book.stok} </td>
+                <td>
+                  <button className="btn btn-info mr-3">Edit</button>
+                  <button className="btn btn-danger">Hapus</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
